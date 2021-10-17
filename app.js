a9252c1dec8af8733a5059e3b4915986c9f61daa
Engines-screaming <karl.ravago@gmail.com>
2021-10-17 16:52:36 -0700

    part 1 complete. used gitignore to hide the api key

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("Let's get this party started!");
+async function searchGif() {
+    try {
+        q = document.querySelector('#search');
+
+        result = await axios.get("http://api.giphy.com/v1/gifs/search",
+            { 
+                params: { 
+                    q: q.value, 
+                    api_key: token
+                },  
+                // headers: { 'Access-Control-Allow-Origin': '*' } 
+            }
+        );
+
+        console.log(result);
+
+    } catch(e) {
+        console.log("request failed")
+        console.log(e);
+    }
+}
+
+submitButton = document.querySelector('#submit');
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    searchGif();
+})
